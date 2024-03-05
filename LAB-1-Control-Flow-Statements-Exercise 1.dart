4c8bf15d941403c1f253a6801f23fc9e44856a86
Eyob Derese <eyobderese123@gmail.com>
2024-03-05 15:31:48 +0300

    LAB-1-Control-Flow-Statements-Exercise 1

---
+++ LAB-1-Control-Flow-Statements-Exercise 1.dart
@@ -0,0 +1,21 @@
+void main() {
+  int DAY = 1;
+
+  switch (DAY) {
+    case 1:
+      print("Monday");
+    case 2:
+      print("Tuesday");
+    case 3:
+      print("Wednesday");
+    case 4:
+      print("Thursday");
+    case 5:
+      print("Friday");
+    case 6:
+      print("Saturday");
+
+    default:
+      print("Sunday");
+  }
+}
