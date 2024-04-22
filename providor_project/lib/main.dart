04b07f85bbc5017651a3d2986173d9e26f2d5395
eyobderese <eyobderese123@gmail.com>
2024-04-22 15:11:11 +0300

    fixing some error

--- providor_project/lib/main.dart
+++ providor_project/lib/main.dart
@@ -3,7 +3,12 @@ import 'package:provider/provider.dart';
 import 'package:providor_project/Provider/counter_providor.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => CounterProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -36,30 +41,29 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Consumer<CounterProvider>(builder: (context, value, child) {
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              IconButton(
-                  onPressed: () {
-                    value.increment();
-                  },
-                  icon: Icon(Icons.add)),
-              Text(value.counter.toString()),
-              IconButton(
-                  onPressed: () {
-                    value.decrement();
-                  },
-                  icon: Icon(Icons.remove)),
-            ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Consumer(
+          builder: (context, CounterProvider value, child) => Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                    onPressed: () {
+                      value.increment();
+                    },
+                    icon: Icon(Icons.add)),
+                Text(value.counter.toString()),
+                IconButton(
+                    onPressed: () {
+                      value.decrement();
+                    },
+                    icon: Icon(Icons.remove)),
+              ],
+            ),
           ),
-        );
-      }),
-    );
+        ));
   }
 }
